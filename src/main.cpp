5a0a5261efa089d573da89c7edd137f00c608980
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-09 09:42:14 -0400

    Add hide rate demon button and add enabled check at the start

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,22 @@ class $modify(LevelInfoLayer) {
     }
 
     void checkToHide() {
+        if (!enabled) return;
         if (Mod::get()->getSettingValue<bool>("only-hide-uncompleted-levels") && GameStatsManager::sharedState()->hasCompletedLevel(m_level)) return;
-        if (enabled && Mod::get()->getSettingValue<bool>("hide-stars-in-level-screen")) {
+        if (Mod::get()->getSettingValue<bool>("hide-stars-in-level-screen")) {
             m_starsLabel->setCString("?");
             m_starsLabel->updateLabel();
         }
-        if (enabled && Mod::get()->getSettingValue<bool>("hide-difficulty-in-level-screen")) {
+        if (Mod::get()->getSettingValue<bool>("hide-difficulty-in-level-screen")) {
             m_difficultySprite->updateDifficultyFrame(0, GJDifficultyName::Short);
+            if (Mod::get()->getSettingValue<bool>("hide-rate-demon-button")) {
+                if (m_demonRateBtn) {
+                    // i don't actually know a reason why someone would want to hide the button
+                    // but be able to click it
+                    // just in case though...
+                    m_demonRateBtn->setOpacity(0);
+                }
+            }
         }
     }
 };
