fa82dc00faac6831f8b0cfa122539f8b73cef6f6
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-09 07:23:00 -0400

    Add hide stars in level screen option

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,35 @@
+
+#include "Geode/loader/SettingV3.hpp"
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
+
+boolean enabled = true;
+
+#include <Geode/modify/LevelInfoLayer.hpp>
+class $modify(LevelInfoLayer) {
+    bool init(GJGameLevel* level, bool challenge) {
+        if (!LevelInfoLayer::init(level, challenge)) return false;
+        checkToHideStarCount();
+        return true;
+    }
+
+    void levelDownloadFinished(GJGameLevel* p0) {
+        LevelInfoLayer::levelDownloadFinished(p0);
+        checkToHideStarCount();
+    }
+
+    void checkToHideStarCount() {
+        if (enabled && Mod::get()->getSettingValue<bool>("hide-stars-in-level-screen")) {
+            m_starsLabel->setCString("?");
+            m_starsLabel->updateLabel();
+        }
+    }
+};
+
+$execute {
+    enabled = Mod::get()->getSettingValue<bool>("enabled");
+    listenForSettingChangesV3("enabled", [](bool enabled) {
+        ::enabled = enabled;
+    });
+}
