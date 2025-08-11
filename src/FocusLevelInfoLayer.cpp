0bd2fa4642b2a5ef01f23ccc83f5eaf7fca316af
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-11 19:01:28 -0400

    Apply enabled to and use one if statement for hiding the demon rate button

--- src/FocusLevelInfoLayer.cpp
+++ src/FocusLevelInfoLayer.cpp
@@ -11,23 +11,24 @@ class $modify(LevelInfoLayer) {
         checkToHideAllElements();
         return true;
     }
-
+    
     void levelDownloadFinished(GJGameLevel* level) {
         LevelInfoLayer::levelDownloadFinished(level);
         checkToHideAllElements();
     }
-
+    
     void checkToHideAllElements() {
         checkToHideDifficultyElements("hide-in-level-screens", m_level, m_difficultySprite, m_starsLabel, m_orbsLabel);
-        if (Mod::get()->getSettingValue<bool>("hide-difficulty")) {
-            if (Mod::get()->getSettingValue<bool>("hide-rate-demon-button")) {
-                if (m_demonRateBtn) {
-                    // i don't actually know a reason why someone would want to hide the button
-                    // but be able to click it
-                    // just in case though...
-                    m_demonRateBtn->setOpacity(0);
-                }
-            }
+        if (
+            Mod::get()->getSettingValue<bool>("enabled") 
+            && Mod::get()->getSettingValue<bool>("hide-difficulty") 
+            && Mod::get()->getSettingValue<bool>("hide-rate-demon-button") 
+            && m_demonRateBtn
+        ) {
+            // i don't actually know a reason why someone would want to hide the button
+            // but be able to click it
+            // just in case though...
+            m_demonRateBtn->setOpacity(0);
         }
     }
 };
