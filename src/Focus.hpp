c3f44d69fae497456f376799b6a249847104a38e
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-11 21:48:09 -0400

    Separate focus declaration and definition

--- src/Focus.hpp
+++ src/Focus.hpp
@@ -4,33 +4,4 @@
 
 using namespace geode::prelude;
 
-#define STANDARD_DEMON_NUMBER 6
-#define NA_NUMBER 0
-
-inline void checkToHideDifficultyElements(std::string setting, GJGameLevel* level, GJDifficultySprite* difficultySprite, CCLabelBMFont* starsLabel, CCLabelBMFont* orbsLabel) {
-    if (!Mod::get()->getSettingValue<bool>("enabled")) return;
-    if (!Mod::get()->getSettingValue<bool>(setting)) return;
-    if (Mod::get()->getSettingValue<bool>("only-hide-rated-levels") && !level->m_stars) return;
-    if (Mod::get()->getSettingValue<bool>("only-hide-uncompleted-levels") && GameStatsManager::sharedState()->hasCompletedLevel(level)) return;
-    if (Mod::get()->getSettingValue<bool>("dont-hide-auto-levels") && level->m_autoLevel) return;
-    std::string hiddenTextReplacement = Mod::get()->getSettingValue<std::string>("hidden-text-replacement");
-    if (Mod::get()->getSettingValue<bool>("demon-mode") && level->m_stars == 10) {
-        difficultySprite->updateDifficultyFrame(STANDARD_DEMON_NUMBER, GJDifficultyName::Short);
-        return;
-    }
-    if (Mod::get()->getSettingValue<bool>("hide-difficulty") && difficultySprite) {
-        difficultySprite->updateDifficultyFrame(NA_NUMBER, GJDifficultyName::Short);
-    }
-    if (Mod::get()->getSettingValue<bool>("hide-stars") && starsLabel) {
-        starsLabel->setString(hiddenTextReplacement.c_str());
-        starsLabel->updateLabel();
-    }
-    // the orbsLabel check is not only for unrated levels but for auto levels which give no orbs
-    // if the level has ? stars and no orbs label i think you can figure out what the difficulty is
-    if (Mod::get()->getSettingValue<bool>("hide-orbs") && orbsLabel) {
-        // orbtained
-        int orbsObtained = GameStatsManager::sharedState()->getAwardedCurrencyForLevel(level);
-        orbsLabel->setString(fmt::format("{}/{}", orbsObtained, hiddenTextReplacement.c_str()).c_str());
-        orbsLabel->updateLabel();
-    }
-}
+void checkToHideDifficultyElements(std::string setting, GJGameLevel* level, GJDifficultySprite* difficultySprite, CCLabelBMFont* starsLabel, CCLabelBMFont* orbsLabel);
